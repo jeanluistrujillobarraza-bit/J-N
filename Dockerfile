ede7f315fd836976df864a502bdf8de727ce1405
jeanluistrujillobarraza-bit <jeanluistrujillobarraza-bit@users.noreply.github.com>
2026-09-12 02:30:31 -0500

    Use standard Spring Mongo customizer and explicit JVM TLS flags

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,6 @@ RUN mvn clean package -DskipTests
 FROM eclipse-temurin:21-jre
 WORKDIR /app
 COPY --from=build /app/target/store-0.0.1-SNAPSHOT.jar app.jar
-ENV JAVA_TOOL_OPTIONS="-Djdk.tls.client.protocols=TLSv1.2 -Dhttps.protocols=TLSv1.2"
+ENV JAVA_OPTS="-Djdk.tls.client.protocols=TLSv1.2 -Dhttps.protocols=TLSv1.2 -Djsse.enableSNIExtension=true"
 EXPOSE 8080
-ENTRYPOINT ["java", "-jar", "app.jar"]
+ENTRYPOINT ["sh", "-c", "java $JAVA_OPTS -jar app.jar"]
--- src/main/java/com/jyn/store/config/MongoConfig.java
+++ src/main/java/com/jyn/store/config/MongoConfig.java
@@ -1,48 +1,24 @@
 package com.jyn.store.config;
 
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.mongo.MongoClientSettingsBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.net.ssl.SSLContext;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class MongoConfig {
 
-    @Value("${spring.data.mongodb.uri}")
-    private String mongoUri;
-
     @Bean
-    public MongoClient mongoClient() {
-        ConnectionString connectionString = new ConnectionString(mongoUri);
-
-        MongoClientSettings.Builder settingsBuilder = MongoClientSettings.builder()
-                .applyConnectionString(connectionString)
-                .applyToSocketSettings(builder -> {
-                    builder.connectTimeout(20, TimeUnit.SECONDS);
-                    builder.readTimeout(20, TimeUnit.SECONDS);
-                })
-                .applyToClusterSettings(builder -> {
-                    builder.serverSelectionTimeout(20, TimeUnit.SECONDS);
-                });
-
-        if (mongoUri.startsWith("mongodb+srv://") || mongoUri.contains("ssl=true")) {
-            try {
-                SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
-                sslContext.init(null, null, null);
-                settingsBuilder.applyToSslSettings(builder -> {
-                    builder.enabled(true);
-                    builder.context(sslContext);
-                });
-            } catch (Exception ignored) {
-            }
-        }
-
-        return MongoClients.create(settingsBuilder.build());
+    public MongoClientSettingsBuilderCustomizer mongoClientSettingsCustomizer() {
+        return builder -> {
+            builder.applyToSocketSettings(socket -> {
+                socket.connectTimeout(30, TimeUnit.SECONDS);
+                socket.readTimeout(30, TimeUnit.SECONDS);
+            });
+            builder.applyToClusterSettings(cluster -> {
+                cluster.serverSelectionTimeout(30, TimeUnit.SECONDS);
+            });
+        };
     }
 }
