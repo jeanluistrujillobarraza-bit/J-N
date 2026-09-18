68a8da59c09c53b1e1e3719814a02acdfaf6cfab
jeanluistrujillobarraza-bit <jeanluistrujillobarraza-bit@users.noreply.github.com>
2026-09-18 02:36:17 -0500

    Fix search by category and keyword case-sensitivity in ProductRepository

--- src/main/java/com/jyn/store/repository/ProductRepository.java
+++ src/main/java/com/jyn/store/repository/ProductRepository.java
@@ -24,7 +24,7 @@ public interface ProductRepository extends MongoRepository<Product, String> {
     @Query("{ 'deleted': { '$ne': true }, '$or': [ { 'name': { '$regex': ?0, '$options': 'i' } }, { 'description': { '$regex': ?0, '$options': 'i' } } ] }")
     List<Product> searchByNameOrDescription(String keyword);
     
-    @Query("{ 'deleted': { '$ne': true }, 'category': ?0, '$or': [ { 'name': { '$regex': ?1, '$options': 'i' } }, { 'description': { '$regex': ?1, '$options': 'i' } } ] }")
+    @Query("{ 'deleted': { '$ne': true }, 'category': { '$regex': ?0, '$options': 'i' }, '$or': [ { 'name': { '$regex': ?1, '$options': 'i' } }, { 'description': { '$regex': ?1, '$options': 'i' } } ] }")
     List<Product> searchByCategoryAndKeyword(String category, String keyword);
 
     @Query("{ 'deleted': { '$ne': true }, 'type': { '$regex': ?0, '$options': 'i' }, '$or': [ { 'name': { '$regex': ?1, '$options': 'i' } }, { 'description': { '$regex': ?1, '$options': 'i' } } ] }")
